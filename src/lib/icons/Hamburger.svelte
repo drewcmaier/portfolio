e06dd1108491c3f00cba797b28bd3c8832de03bf
Drew Maier <drewcmaier@gmail.com>
2023-01-02 17:16:29 -0500

    fix: thinner mobile icons

--- src/lib/icons/Hamburger.svelte
+++ src/lib/icons/Hamburger.svelte
@@ -1,7 +1,7 @@
 <svg viewBox="0 0 50 50" width="32" height="28">
-	<rect y="0" width="100" height="10" />
-	<rect y="20" width="100" height="10" />
-	<rect y="40" width="100" height="10" />
+	<rect y="0" width="100" height="4" />
+	<rect y="20" width="100" height="4" />
+	<rect y="40" width="100" height="4" />
 </svg>
 
 <style>
