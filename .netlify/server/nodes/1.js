be7773ecb7ead3b05bc4d6e3320f587b36534745
Drew Maier <drewcmaier@gmail.com>
2023-11-19 11:59:11 -0500

    chore: remove netlify assets

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.32573c57.js","_app/immutable/chunks/scheduler.2cdf953d.js","_app/immutable/chunks/index.452094d6.js","_app/immutable/chunks/stores.6f99ba77.js","_app/immutable/chunks/singletons.9e5ccbe5.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/experience/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.3d17e3eb.js","_app/immutable/chunks/scheduler.2cdf953d.js","_app/immutable/chunks/index.452094d6.js"];
-export const stylesheets = ["_app/immutable/assets/3.dc9ab6e1.css"];
-export const fonts = [];
