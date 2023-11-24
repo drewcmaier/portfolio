47a741a8b55f7bcaa4463ddb896692e84e154f63
Drew Maier <drewcmaier@gmail.com>
2023-11-24 15:34:14 -0500

    Revert "fix: use #key to fade in ProjectCard"
    This reverts commit e8869b4a87e5efad5ccd7211bcb50b4cf879f720.

--- src/routes/experience/ProjectCard.svelte
+++ src/routes/experience/ProjectCard.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
 	import VisibilityObserver from '../../lib/VisibilityObserver.svelte';
 
 	export let heading: string;
@@ -7,19 +6,19 @@
 </script>
 
 <VisibilityObserver bind:isVisible threshold={0.01}>
-	{#key isVisible}
-		<section in:fade={{ duration: 500 }}>
-			<h3>{heading}</h3>
-			<div class="project-card-content">
-				<slot />
-			</div>
-		</section>
-	{/key}
+	<section class:project-card-transition-in={isVisible}>
+		<h3>{heading}</h3>
+		<div class="project-card-content">
+			<slot />
+		</div>
+	</section>
 </VisibilityObserver>
 
 <style>
 	section {
 		padding: var(--spacing-4);
+		opacity: 0;
+		transition: opacity 0.5s;
 	}
 
 	section h3 {
@@ -39,5 +38,9 @@
 		display: flex;
 		flex-direction: column;
 		gap: var(--spacing-4);
+	}
+
+	.project-card-transition-in {
+		opacity: 1;
 	}
 </style>
