74f814e1963d7d1ed9f41c5da774b4c50c62f337
Drew Maier <drewcmaier@gmail.com>
2025-03-01 18:15:07 -0500

    feat: add fun secret page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
+	import { page } from '$app/state';
 	import { inject } from '@vercel/analytics';
 
 	import Footer from './Footer.svelte';
@@ -12,31 +13,37 @@
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<Nav />
+{#if page.route.id === '/fun'}
+	<main class="root-content">
+		{@render children()}
+	</main>
+{:else}
+	<Nav />
 
-<main class="root-content">
-	{@render children()}
-</main>
+	<main class="root-content">
+		{@render children()}
+	</main>
 
-<Footer />
+	<Footer />
 
-<style>
-	.root-content {
-		padding: var(--spacing-3);
-		background-color: var(--color-background);
-
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-
-		padding: var(--spacing-4);
-		/* shift down content to account for floating header  */
-		padding-block-start: calc(70px + var(--spacing-4));
-	}
-
-	@media screen and (min-width: 32rem) {
+	<style>
 		.root-content {
-			padding-block-start: var(--spacing-4);
+			padding: var(--spacing-3);
+			background-color: var(--color-background);
+
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+
+			padding: var(--spacing-4);
+			/* shift down content to account for floating header  */
+			padding-block-start: calc(70px + var(--spacing-4));
 		}
-	}
-</style>
+
+		@media screen and (min-width: 32rem) {
+			.root-content {
+				padding-block-start: var(--spacing-4);
+			}
+		}
+	</style>
+{/if}
---
+++ src/routes/fun/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import brutalist from './images/brutalist.png';
+	import completeunknown from './images/complete-unknown.png';
+	import dune2 from './images/dune2.png';
+	import perez from './images/perez.png';
+	import wicked from './images/wicked.png';
+</script>
+
+<article>
+	<h1>The Dogcademy Awards 2025</h1>
+	<img src={brutalist} alt="" />
+	<img src={completeunknown} alt="" />
+	<img src={dune2} alt="" />
+	<img src={perez} alt="" />
+	<img src={wicked} alt="" />
+</article>
+
+<style>
+	article {
+		background: white;
+		display: flex;
+		flex-direction: column;
+		margin-inline: auto;
+		margin-block: 4rem;
+
+		padding-inline: 8rem;
+		padding-block-start: 2rem;
+		padding-block-end: 4rem;
+		max-width: fit-content;
+	}
+
+	h1 {
+		font-family: 'Futura';
+		font-size: 4rem;
+		color: #e9c760;
+		/* filter: drop-shadow(); */
+		text-transform: uppercase;
+		text-align: center;
+		margin-block-end: 2rem;
+	}
+
+	img:not(:last-of-type) {
+		margin-block-end: 4rem;
+	}
+</style>
