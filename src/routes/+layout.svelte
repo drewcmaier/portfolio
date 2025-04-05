3fbde1d979957caeb74aeada1b63a6e4ed6d5be9
Drew Maier <drewcmaier@gmail.com>
2025-04-05 18:03:29 -0400

    feat: remove fun

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,37 +13,31 @@
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-{#if page.route.id === '/fun'}
-	<main class="root-content">
-		{@render children()}
-	</main>
-{:else}
-	<Nav />
+<Nav />
 
-	<main class="root-content">
-		{@render children()}
-	</main>
+<main class="root-content">
+	{@render children()}
+</main>
 
-	<Footer />
+<Footer />
 
-	<style>
+<style>
+	.root-content {
+		padding: var(--spacing-3);
+		background-color: var(--color-background);
+
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+
+		padding: var(--spacing-4);
+		/* shift down content to account for floating header  */
+		padding-block-start: calc(70px + var(--spacing-4));
+	}
+
+	@media screen and (min-width: 32rem) {
 		.root-content {
-			padding: var(--spacing-3);
-			background-color: var(--color-background);
-
-			flex: 1;
-			display: flex;
-			flex-direction: column;
-
-			padding: var(--spacing-4);
-			/* shift down content to account for floating header  */
-			padding-block-start: calc(70px + var(--spacing-4));
+			padding-block-start: var(--spacing-4);
 		}
-
-		@media screen and (min-width: 32rem) {
-			.root-content {
-				padding-block-start: var(--spacing-4);
-			}
-		}
-	</style>
-{/if}
+	}
+</style>
