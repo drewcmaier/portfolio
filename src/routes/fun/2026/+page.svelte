63829e823d500b7b30598cc71c40510f6964546b
Drew Maier <drewcmaier@gmail.com>
2026-03-10 20:38:06 -0400

    feat: more fun

--- src/routes/fun/2026/+page.svelte
+++ src/routes/fun/2026/+page.svelte
@@ -3,9 +3,10 @@
 	import frankenstein from './images/frankenstein.jpg';
 	import hamnet from './images/hamnet.jpg';
 	import martysupreme from './images/martysupreme.jpg';
+	import onebattle from './images/onebattle.jpg';
 	import sinners from './images/sinners.jpg';
 
-	const images = [frankenstein, hamnet, martysupreme, sinners];
+	const images = [frankenstein, hamnet, martysupreme, onebattle, sinners];
 </script>
 
 <AwardsPage {images} year={2026} />
