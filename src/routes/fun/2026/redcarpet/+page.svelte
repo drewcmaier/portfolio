1e87c9e2b829f5343507ce0aad0c502e70141f26
Drew Maier <drewcmaier@gmail.com>
2026-03-15 17:24:29 -0400

    feat: fun live

---
+++ src/routes/fun/2026/redcarpet/+page.svelte
@@ -0,0 +1,575 @@
+<script lang="ts">
+	import pearl from './images/dress1.jpg';
+	import lulu from './images/dress2.jpg';
+	import royal from './images/suit1.jpg';
+
+	const galleryItems = [
+		{
+			id: 1,
+			image: pearl,
+			title: 'Pearl',
+			caption:
+				'The beautiful baby embraces her French roots in a stunning custom Chanel gown and pink beret'
+		},
+		{
+			id: 2,
+			image: royal,
+			title: 'Royal',
+			caption: 'This handsome man brightens up the red carpet in a Versace tuxedo'
+		},
+		{
+			id: 3,
+			image: lulu,
+			title: 'Lulu',
+			caption: 'The conniving Chorkie spreads doom and gloom in a velvet Dolce & Gabbana dress'
+		}
+	];
+
+	const totalPhotos = galleryItems.length;
+	const publishDate = 'Mar 15, 2026 5:03 PM';
+</script>
+
+<svelte:head>
+	<title>Red Carpet 2026</title>
+	<meta name="description" content="Red Carpet 2026" />
+	<meta property="og:title" content="Red Carpet 2026" />
+	<meta property="og:description" content="Red Carpet 2026" />
+	<meta
+		property="og:image"
+		content="https://akns-images.eonline.com/eol_images/Entire_Site/20260315/9401470c-7cc1-4801-92ad-48a87d655f5c_1773608743.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top"
+	/>
+	<meta property="og:url" content="https://www.eonline.com/photos/38129/pre-oscars-2026-parties" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Red Carpet 2026" />
+	<meta name="twitter:description" content="Red Carpet 2026" />
+	<meta
+		name="twitter:image"
+		content="https://akns-images.eonline.com/eol_images/Entire_Site/20260315/9401470c-7cc1-4801-92ad-48a87d655f5c_1773608743.jpg?fit=around%7C1080:1080&output-quality=90&crop=1080:1080;center,top"
+	/>
+</svelte:head>
+
+<div class="page">
+	<header class="navigation">
+		<nav class="nav-bar">
+			<div class="nav-bar__top">
+				<div class="nav__container">
+					<div class="nav-bar__logo">
+						<a href="/" data-analytics-linklocation="navigation" data-analytics-linkname="e-logo">
+							<svg
+								class="nav-bar__logo-img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 745 441"
+							>
+								<path
+									d="M679.45,141.92c0-8.95,4.56-14.3,13.45-14.3,10.73,0,13,8.78,13,16.14v29.89L745,163.1V144.87c0-32.51-18.94-53.52-50.59-53.52-17.8,0-30.84,5.81-40.58,16.81-9,10.12-13.69,24.34-13.69,41.13,0,57.25,64.92,102,64.92,150.37,0,10.86-5,16.14-15.43,16.14-9.7,0-14.62-5.8-14.62-17.25V266.16l-39.53,10.57v20.72c0,33,19.71,53.52,53.87,53.52,14.94,0,27-4,36.74-12.48,11.47-10.06,18.28-23,18.28-41.71,0-61.14-64.92-104.34-64.92-154.86"
+									fill="currentColor"
+								></path>
+								<polygon
+									points="583.43 95.46 573.22 181.39 566.58 259.03 565.24 214.59 551.1 95.46 517.87 95.46 507.57 182.22 501 259.04 499.72 214.22 485.62 95.46 444.72 95.46 478.28 346.85 519.17 346.85 531.7 249.96 531.64 205.69 536.25 242.08 549.8 346.85 590.77 346.85 624.33 95.46 583.43 95.46"
+									fill="currentColor"
+								></polygon>
+								<polygon
+									points="268.77 95.46 268.77 181.91 272.86 239.96 260.72 187.25 228.5 95.46 192.15 95.46 192.15 346.85 233.06 346.85 233.06 265.42 233.01 265.42 228.6 200.31 235.72 232.57 275.84 346.85 309.68 346.85 309.68 95.46 268.77 95.46"
+									fill="currentColor"
+								></polygon>
+								<polygon
+									points="377.59 241.61 416.95 241.61 416.95 200.71 377.59 200.71 377.59 136.37 421.47 136.37 428.23 95.46 336.69 95.46 336.69 346.86 421.47 346.86 428.23 305.95 377.59 305.95 377.59 241.61"
+									fill="currentColor"
+								></polygon>
+								<rect x="11.14" y="345.75" width="111.42" height="95.25" fill="#e8f3f3"></rect>
+								<polygon
+									points="131.82 61.27 133.95 0 0 0 11.19 317.63 122.79 317.63 124.94 259.53 71.66 259.53 71.66 177.91 127.71 177.91 129.6 124.27 72.48 124.27 72.48 61.27 131.82 61.27"
+									fill="#e8f3f3"
+								></polygon>
+							</svg>
+						</a>
+					</div>
+					<div class="nav-bar-icons">
+						<label for="queryly_toggle" class="queryly_toggle">
+							<div class="nav-bar-icons__search nav-bar-icons__icon queryly_search_button">
+								<svg
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="white"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<g clip-path="url(#clip0_742_38815)">
+										<path
+											fill-rule="evenodd"
+											clip-rule="evenodd"
+											d="M9.01 18.02c1.84 0 3.54-.55 4.97-1.49L21.45 24 24 21.45l-7.47-7.47a8.977 8.977 0 0 0 1.49-4.97C18.02 4.03 13.99 0 9.01 0S0 4.03 0 9.01s4.03 9.01 9.01 9.01Zm6.22-9.01c0 3.43-2.78 6.22-6.22 6.22s-6.22-2.78-6.22-6.22a6.22 6.22 0 0 1 12.44 0Z"
+										></path>
+									</g>
+									<defs>
+										<clipPath id="clip0_742_38815">
+											<rect width="24" height="24" fill="white"></rect>
+										</clipPath>
+									</defs>
+								</svg>
+							</div>
+						</label>
+						<button class="nav-bar-icons__icon nav-bar-icons__hamburger">
+							<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M24 4H0V2H24V4Z"></path>
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M24 13H0V11H24V13Z"></path>
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M24 22H0V20H24V22Z"></path>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="nav-bar__bottom">
+				<div class="nav__container">
+					<div class="nav-bar__hot-topic-links">
+						<a class="nav-bar__hot-topic-link" href="https://www.eonline.com/news">News</a>
+						<a class="nav-bar__hot-topic-link" href="https://www.eonline.com/photos">Photos</a>
+						<a class="nav-bar__hot-topic-link" href="https://www.eonline.com/videos">Videos</a>
+						<a class="nav-bar__hot-topic-link" href="/news/shop">Shopping</a>
+						<a class="nav-bar__hot-topic-link" href="/news/oscars">Oscars 2026</a>
+						<a class="nav-bar__hot-topic-link" href="/news/couples">Couples</a>
+						<a class="nav-bar__hot-topic-link" href="/news/influencer">Influencers</a>
+						<a class="nav-bar__hot-topic-link" href="/shows/kardashians">Kardashians</a>
+						<a class="nav-bar__hot-topic-link" href="/news/royals">Royals</a>
+						<a class="nav-bar__hot-topic-link" href="/news/tv">TV Scoop</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+
+	<div class="theme-wrap">
+		<div class="gallery-detail-container gallery-detail-dark-mode">
+			<main class="container gallery-detail">
+				<header class="columns is-desktop">
+					<div class="column is-10-desktop is-offset-1-desktop gallery-detail__header-content">
+						<h1 class="gallery-detail__title">Red Carpet 2026</h1>
+						<div class="gallery-detail__total-photo-count">
+							<div class="gallery-detail__camera-icon">
+								<svg class="camera-icon-svg" viewBox="0 0 20 18" fill="currentColor">
+									<path
+										d="M5.333.333h9.334V2.81h4.666v14.666H.667V2.81h4.666V.333zM10 5.286a4.667 4.667 0 100 9.333 4.667 4.667 0 000-9.333z"
+										fill-rule="evenodd"
+									/>
+								</svg>
+							</div>
+							<div class="gallery-detail__total-photo-count-amount">{totalPhotos} photos</div>
+						</div>
+						<time class="gallery-detail__timestamp">{publishDate}</time>
+						<div class="social-share social-share--gallery-subhead">
+							<button class="social-share__fb-button social-share__button">
+								<svg class="social-share__button__svg" viewBox="0 0 18 18" fill="currentColor">
+									<path
+										d="M9.705 17.258H6.472v-8.63H4.314V5.656h2.158l-.004-1.753C6.468 1.476 7.126 0 9.984 0h2.379v2.974h-1.487c-1.113 0-1.166.416-1.166 1.192l-.005 1.488h2.675l-.316 2.974H9.707l-.002 8.63z"
+									/>
+								</svg>
+							</button>
+							<button class="social-share__tw-button social-share__button">
+								<svg class="social-share__button__svg" viewBox="0 0 18 18" fill="currentColor">
+									<path
+										d="M10.27 7.307L16.695 0h-1.522L9.594 6.345 5.14 0H0l6.737 9.595L0 17.258h1.523l5.89-6.7 4.706 6.7h5.139l-6.988-9.95zM8.186 9.68l-.682-.955L2.07 1.12h2.338l4.384 6.136.682.955 5.698 7.975h-2.338l-4.65-6.508z"
+									/>
+								</svg>
+							</button>
+							<div class="social-share__link">
+								<button class="social-share__link-button social-share__button">
+									<svg class="social-share__button__svg" viewBox="0 0 18 18" fill="currentColor">
+										<path
+											fill-rule="evenodd"
+											clip-rule="evenodd"
+											d="M9.186 1.978a4.314 4.314 0 016.101 6.1l-.009.01-2.157 2.157a4.314 4.314 0 01-6.506-.466.72.72 0 111.152-.862 2.876 2.876 0 004.337.31l2.153-2.152a2.876 2.876 0 00-4.068-4.066L8.956 4.235a.72.72 0 01-1.014-1.02l1.237-1.23.007-.007z"
+										/>
+										<path
+											fill-rule="evenodd"
+											clip-rule="evenodd"
+											d="M5.68 6.021a4.314 4.314 0 014.963 1.458.719.719 0 11-1.152.861 2.876 2.876 0 00-4.337-.31L3 10.182a2.876 2.876 0 004.067 4.067l1.225-1.225a.72.72 0 011.017 1.017l-1.23 1.23-.009.009a4.315 4.315 0 01-6.1-6.1l.008-.01 2.157-2.157a4.315 4.315 0 011.544-.992z"
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+					</div>
+				</header>
+
+				<div class="gallery-detail__body">
+					<div class="gallery-detail__main-content">
+						<div class="columns is-desktop photo-items">
+							<div class="column is-7-desktop is-offset-1-desktop photo-items__content">
+								{#each galleryItems as item}
+									<div class="photo-item" id="photo-{item.id}">
+										<figure>
+											<div class="photo-item__photo-content">
+												<div class="photo-item__pinterest">
+													<a
+														data-pin-do="buttonPin"
+														data-pin-tall="true"
+														data-pin-round="true"
+														href="https://www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.eonline.com%2Fphotos%2F38129%2Fpre-oscars-2026-parties%23{item.id}&amp;media={item.image}&amp;description={item.title} from Red Carpet 2026 on E! Online"
+													>
+														<img
+															src="//assets.pinterest.com/images/pidgets/pinit_fg_en_round_red_32.png"
+															alt="Pin It"
+														/>
+													</a>
+												</div>
+												<span class="thumb thumb--inview">
+													<img
+														srcSet="{item.image}?fit=around%7C776:1035&amp;output-quality=90&amp;crop=776:1035;center,top 1x, {item.image}?fit=around%7C768:1024&amp;output-quality=90&amp;crop=768:1024;center,top 2x"
+														src="{item.image}?fit=around%7C776:1035&amp;output-quality=90&amp;crop=776:1035;center,top"
+														alt={item.title}
+													/>
+												</span>
+												<div class="photo-item__number-tag">{item.id}/{totalPhotos}</div>
+											</div>
+										</figure>
+										<div class="photo-item__caption">
+											<h2 class="photo-item__title"><p>{item.title}</p></h2>
+										</div>
+										<p class="photo-item__caption-text">{item.caption}</p>
+										<hr class="photo-item__divider" />
+									</div>
+								{/each}
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+</div>
+
+<style>
+	/* Basic resets and global styles */
+	* {
+		box-sizing: border-box;
+	}
+
+	.page {
+		min-height: 100vh;
+	}
+
+	/* Navigation styles */
+	.navigation {
+		background: #000;
+		color: white;
+	}
+
+	.nav-bar {
+		width: 100%;
+	}
+
+	.nav-bar__top {
+		background: #000;
+	}
+
+	.nav-bar__bottom {
+		background: white;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		overflow-x: auto;
+		-webkit-overflow-scrolling: touch;
+	}
+
+	.nav-bar__hot-topic-links {
+		min-width: 100%;
+	}
+
+	.nav-bar__hot-topic-links::-webkit-scrollbar {
+		height: 6px;
+	}
+
+	.nav-bar__hot-topic-links::-webkit-scrollbar-thumb {
+		background: rgba(0, 0, 0, 0.2);
+		border-radius: 999px;
+	}
+
+	.nav__container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		max-width: 1200px;
+		margin: 0 auto;
+		padding: 0.75rem 1rem;
+	}
+
+	.nav-bar__logo a {
+		color: white;
+		text-decoration: none;
+	}
+
+	.nav-bar__logo-img {
+		width: 60px;
+		height: auto;
+		color: #4cafe5;
+	}
+
+	.nav-bar__hot-topic-links {
+		display: flex;
+		gap: 1.25rem;
+		overflow-x: auto;
+		-webkit-overflow-scrolling: touch;
+		padding: 0.5rem 0;
+	}
+
+	.nav-bar__hot-topic-link {
+		color: #111;
+		text-decoration: none;
+		font-size: 0.85rem;
+		font-weight: 700;
+		text-transform: uppercase;
+		letter-spacing: 0.02em;
+		white-space: nowrap;
+		position: relative;
+	}
+
+	.nav-bar__hot-topic-link:not(:first-child)::before {
+		content: '';
+		position: absolute;
+		left: -0.75rem;
+		top: 50%;
+		transform: translateY(-50%);
+		width: 1px;
+		height: 14px;
+		background: rgba(0, 0, 0, 0.2);
+	}
+
+	.nav-bar-icons {
+		display: flex;
+		gap: 1rem;
+		color: white;
+	}
+
+	.nav-bar-icons__icon {
+		background: none;
+		border: none;
+		color: white;
+		cursor: pointer;
+		padding: 0.5rem;
+	}
+
+	.nav-bar-icons__icon svg {
+		fill: currentColor !important;
+	}
+
+	/* Gallery detail styles */
+	.theme-wrap {
+		background: #000;
+	}
+
+	.gallery-detail-container {
+		max-width: 1200px;
+		margin: 0 auto;
+		color: white;
+	}
+
+	.gallery-detail__header-content {
+		text-align: left;
+		max-width: 900px;
+		padding: 1rem 1rem 0 1rem;
+	}
+
+	.gallery-detail__title {
+		font-size: 2.5rem;
+		font-weight: 400;
+		margin-bottom: 0.25rem;
+		color: white;
+	}
+
+	.gallery-detail__total-photo-count {
+		display: flex;
+		color: #bc2a9b;
+		font-size: 1rem;
+	}
+
+	.gallery-detail__camera-icon {
+		width: 1rem;
+		height: 1rem;
+		position: relative;
+		top: 2px;
+	}
+
+	.gallery-detail__camera-icon svg {
+		color: #bc2a9b;
+		fill: #bc2a9b;
+	}
+
+	.gallery-detail__total-photo-count-amount {
+		padding-left: 0.5rem;
+		text-transform: uppercase;
+	}
+
+	.gallery-detail__timestamp {
+		color: #9f9f9f;
+		font-size: 1rem;
+		margin-top: 0.5rem;
+		display: block;
+		text-transform: uppercase;
+		max-width: 900px;
+	}
+
+	.social-share {
+		margin: 0 0 2rem;
+		display: flex;
+		justify-content: flex-start;
+		gap: 2rem;
+	}
+
+	.social-share__button {
+		background: transparent;
+		border: none;
+		padding: 0;
+		width: 18px;
+		height: 18px;
+	}
+
+	.social-share__button__svg {
+		width: 1rem;
+		height: 1rem;
+		fill: white;
+	}
+
+	.social-share__button:hover {
+		cursor: pointer;
+	}
+
+	.social-share__link {
+		display: flex;
+		align-items: center;
+		font-size: 1.2rem;
+		font-weight: 700;
+		letter-spacing: 0.25px;
+		color: #1e1e1e;
+		gap: 1rem;
+	}
+
+	.social-share--gallery-subhead {
+		margin: 2rem 0;
+	}
+
+	@media screen and (max-width: 757px) {
+		.social-share--gallery-subhead {
+			margin: 1rem 0;
+		}
+	}
+
+	.gallery-detail-dark-mode.gallery-detail-container {
+		background-color: #000;
+	}
+
+	/* Photo items */
+	.photo-items__content {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	figure {
+		margin: 0 2rem;
+		padding: 0rem;
+	}
+
+	.photo-item__photo-content {
+		position: relative;
+	}
+
+	.photo-item__pinterest {
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+		z-index: 2;
+	}
+
+	.photo-item__pinterest img {
+		width: 32px;
+		height: 32px;
+		display: block;
+	}
+
+	.thumb {
+		display: block;
+		position: relative;
+		width: 100%;
+		background: #111;
+	}
+
+	.thumb img {
+		width: 100%;
+		height: auto;
+		display: block;
+	}
+
+	.photo-item__number-tag {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		width: auto;
+		padding: 0 0.6rem;
+		background-color: #fff;
+		line-height: 2.1;
+		text-align: center;
+		font-size: 0.75rem;
+		color: #1e1e1e;
+		opacity: 0.9;
+		z-index: 2;
+	}
+
+	.photo-item__caption {
+		display: flex;
+		justify-content: space-between;
+		margin-top: 1rem;
+	}
+
+	.photo-item__title {
+		color: #1e1e1e;
+		font-size: 2rem;
+		font-weight: bold;
+		line-height: 1.3;
+		padding-inline: 1rem;
+	}
+
+	.photo-item__caption-text {
+		font-size: 1rem;
+		line-height: 2;
+		padding-inline: 1rem;
+	}
+
+	.photo-item__title p {
+		margin: 0;
+	}
+
+	.photo-item__divider {
+		margin: 0.5rem 1rem 1rem 1rem;
+		width: 60px;
+		height: 1px;
+		background-color: #bc2a9b;
+		border: none;
+		display: block;
+	}
+
+	/* Dark mode overrides */
+	.gallery-detail-dark-mode .photo-item .thumb {
+		background-color: hsla(0, 0%, 100%, 0.1);
+	}
+
+	.gallery-detail-dark-mode .photo-item__title {
+		color: #fff;
+	}
+
+	/* Responsive */
+	@media (max-width: 768px) {
+		.gallery-detail__title {
+			font-size: 2rem;
+		}
+
+		.photo-items__content {
+			padding: 0;
+		}
+	}
+
+	@media (max-width: 757px) {
+		.photo-item__photo-content {
+			margin-left: -2rem;
+			margin-right: -2rem;
+		}
+	}
+</style>
